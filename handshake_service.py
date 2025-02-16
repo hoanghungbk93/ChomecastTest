d3dfc0574f2c3dce41f234e5cc9fddf116c8a48d
hoanghungbk93 <hch.bkhn@gmail.com>
2025-02-16 17:55:06 +0700

    add websocket test

--- handshake_service.py
+++ handshake_service.py
@@ -5,20 +5,10 @@ import json
 import os
 import subprocess
 from datetime import datetime
-import eventlet
-eventlet.monkey_patch()
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-
-@socketio.on("connect")
-def handle_connect():
-    print(f"✅ Client {request.sid} connected!")
-
-@socketio.on("disconnect")
-def handle_disconnect():
-    print(f"❌ Client {request.sid} disconnected!")
 
 # Paths to the JSON files
 VERIFIED_IPS_FILE = '/opt/verified_ips.json'
